f12f87b41ca076a555ade4157dce145828d3fae9
Naveen Kn <naveen.kn@ros.com>
2017-11-11 16:54:18 -0800

    reformmating applied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
   <div class="jumbotron" align="center">
-  <h1>Unsaved Changes Demo!</h1>
+  <h1>How to track unsaved changes!</h1>
  </div>
   <div  align="center">
   <nav>
-    <a routerLink="/page1" routerLinkActive="active">Name</a>
-    <a routerLink="/page2" routerLinkActive="active">Email</a>
+    <a routerLink="/page1" routerLinkActive="active">Page1 </a>
+    <a routerLink="/page2" routerLinkActive="active">Page2 </a>
   </nav>
   <router-outlet></router-outlet>
   </div>
@@ -17,5 +17,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  editName='';
+  editName = '';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DialogService } from './dialog.service';
-import { Confirmation } from './confirmation.component';
-import { CanDeactivateGuard } from './can-deactivate-guard.service';
-import { FormsModule } from '@angular/forms';
-import { Page2Component } from './page2.component';
-import { Page1Component } from './page1.component';
-import { RouterModule, Routes } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {DialogService} from './dialog.service';
+import {Confirmation} from './confirmation.component';
+import {CanDeactivateGuard} from './can-deactivate-guard.service';
+import {FormsModule} from '@angular/forms';
+import {Page2Component} from './page2.component';
+import {Page1Component} from './page1.component';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 
 const appRoutes: Routes = [
-  { path: 'page1', component: Page1Component, canDeactivate: [CanDeactivateGuard] },
-  { path: 'page2', component: Page2Component, canDeactivate: [CanDeactivateGuard] }
+  {path: 'page1', component: Page1Component, canDeactivate: [CanDeactivateGuard]},
+  {path: 'page2', component: Page2Component, canDeactivate: [CanDeactivateGuard]}
 ];
 
 @NgModule({
@@ -32,4 +32,5 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent],
   entryComponents: [Confirmation]
 })
-export class AppModule { }
+export class AppModule {
+}
